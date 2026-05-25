4c36b8c0f49a27ea619a592838f3060f0f4b2850
westari <westari@users.noreply.github.com>
2026-05-25 13:35:06 -0400

    Remove Rork from metro config

--- expo/metro.config.js
+++ expo/metro.config.js
@@ -1,6 +1,3 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const { withRorkMetro } = require("@rork-ai/toolkit-sdk/metro");
-
 const config = getDefaultConfig(__dirname);
-
-module.exports = withRorkMetro(config);
+module.exports = config;
